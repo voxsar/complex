849601f9e5479174250f1d3b2e1551cdd1589853
Miyuru Sangith Dharmage <voxsar@gmail.com>
2025-08-31 19:04:15 +0530

    feat: add blog detail view

---
+++ frontend/src/views/BlogDetail.vue
@@ -0,0 +1,205 @@
+<template>
+  <div class="blog-pagev1">
+    <div id="sns_main" class="col-md-9 col-main">
+      <div id="sns_mainmidle">
+        <div class="blogs-page">
+          <div class="postWrapper v1">
+            <a class="post-img" href="#">
+              <img :src="placeholder" alt="" />
+            </a>
+            <div class="date">
+              <span class="poster">JUL 12, 2015</span>
+              <div class="post-info">
+                <div class="postDetails">Post by: KomodoArt</div>
+              </div>
+            </div>
+            <div class="post-title">
+              <a href="#">Donec scelerisque quam vitae est.</a>
+            </div>
+            <div class="post-content">
+              <p class="text1">
+                Duis imperdiet diam pharetra nisi. Fusce accumsan. Fusce adipiscing, felis non ornare egestas, risus elit placerat mauris, in mollis ante erat quis nisi. Quisque sed ipsum. Vestibulum eu risus. hasellus tempus massa aliquet urna. Integer fringilla quam eget dolor.
+              </p>
+              <p class="text2">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus
+              </p>
+              <p class="text3">
+                Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia.
+              </p>
+              <p class="text4">
+                Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+              </p>
+              <p class="text5">
+                The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham. "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+              </p>
+              <p class="text5">
+                "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"
+              </p>
+              <p class="text5">
+                "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."
+              </p>
+              <div class="blog-social">
+                <p class="blog-tt"><span class="bl-cl">Tags:</span> Creative, Table, Chair, Furniture, Fashion wood .</p>
+                <span class="bl-cl">Share:</span>
+                <ul>
+                  <li>
+                    <a class="fa fa-facebook" href="#" target="_self"> </a>
+                  </li>
+                  <li>
+                    <a class="fa fa-twitter" href="#" target="_self"> </a>
+                  </li>
+                  <li>
+                    <a class="fa fa-google-plus" href="#" target="_self"> </a>
+                  </li>
+                  <li>
+                    <a class="fa fa-tencent-weibo" href="#" target="_self"> </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="blog-coments">
+              <h6>4 Comment</h6>
+              <div class="blog-content">
+                <div class="blog-coments-img">
+                  <img :src="placeholder" alt="" />
+                </div>
+                <div class="blog-coments-emma">
+                  <div class="title">
+                    <p>
+                      <span class="title1">Emma </span>
+                      <span class="title2">- Studen</span>
+                    </p>
+                  </div>
+                  <div class="depcisen">
+                    No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur.
+                  </div>
+                  <div class="bottom-cm">
+                    <p>
+                      <span class="title3">December 15, at 14h:20,</span>
+                      <span class="title4">Reply</span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="blog-pdleft">
+                <div class="blog-content referp">
+                  <div class="blog-coments-img">
+                    <img :src="placeholder" alt="" />
+                  </div>
+                  <div class="blog-coments-emma">
+                    <div class="title">
+                      <p>
+                        <span class="title1">Emma </span>
+                        <span class="title2">- Studen</span>
+                      </p>
+                    </div>
+                    <div class="depcisen">
+                      No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur.
+                    </div>
+                    <div class="bottom-cm">
+                      <p>
+                        <span class="title3">December 15, at 14h:20,</span>
+                        <span class="title4">Reply</span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div class="blog-content referp">
+                  <div class="blog-coments-img">
+                    <img :src="placeholder" alt="" />
+                  </div>
+                  <div class="blog-coments-emma">
+                    <div class="title">
+                      <p>
+                        <span class="title1">Emma </span>
+                        <span class="title2">- Studen</span>
+                      </p>
+                    </div>
+                    <div class="depcisen">
+                      No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur.
+                    </div>
+                    <div class="bottom-cm">
+                      <p>
+                        <span class="title3">December 15, at 14h:20,</span>
+                        <span class="title4">Reply</span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="blog-content">
+                <div class="blog-coments-img">
+                  <img :src="placeholder" alt="" />
+                </div>
+                <div class="blog-coments-emma">
+                  <div class="title">
+                    <p>
+                      <span class="title1">Emma </span>
+                      <span class="title2">- Studen</span>
+                    </p>
+                  </div>
+                  <div class="depcisen">
+                    No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur.
+                  </div>
+                  <div class="bottom-cm">
+                    <p>
+                      <span class="title3">December 15, at 14h:20,</span>
+                      <span class="title4">Reply</span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="blog-content">
+                <div class="blog-coments-img">
+                  <img :src="placeholder" alt="" />
+                </div>
+                <div class="blog-coments-emma">
+                  <div class="title">
+                    <p>
+                      <span class="title1">Emma </span>
+                      <span class="title2">- Studen</span>
+                    </p>
+                  </div>
+                  <div class="depcisen">
+                    No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur.
+                  </div>
+                  <div class="bottom-cm">
+                    <p>
+                      <span class="title3">December 15, at 14h:20,</span>
+                      <span class="title4">Reply</span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <h1 class="onenot">
+              Leave a comment
+            </h1>
+            <form class="smart-green">
+              <p class="one2">
+                <input id="name" type="text" placeholder="Name (*)" name="name" />
+                <input id="email1" type="email" placeholder="Email (*)" name="email" />
+                <input id="email2" type="email" placeholder="Your website" name="email" />
+              </p>
+              <p class="one3">
+                <textarea id="message" placeholder="Comment" name="message"></textarea>
+                <input class="button" type="button" value="Submit" />
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const placeholder = new URL('@/assets/vue.svg', import.meta.url).href;
+</script>
+
+<style scoped src="@/assets/less/blog.less" lang="less"></style>
+
