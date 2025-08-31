096743b788da8d8f54bd37351f9862e11a041f56
Miyuru Sangith Dharmage <voxsar@gmail.com>
2025-08-31 08:45:53 +0530

    feat: extend customer wishlist

--- admin/src/views/customers/CustomerWishlist.vue
+++ admin/src/views/customers/CustomerWishlist.vue
@@ -6,9 +6,10 @@
       <button @click="addItem">Add</button>
     </div>
     <ul>
-      <li v-for="id in wishlist.productIds" :key="id">
-        {{ id }}
-        <button @click="removeItem(id)">Remove</button>
+      <li v-for="product in products" :key="product.id">
+        {{ product.title || product.id }}
+        <button @click="removeItem(product.id)">Remove</button>
+        <button @click="transferToOrder(product.id)">Transfer to Order</button>
       </li>
     </ul>
   </div>
@@ -17,16 +18,27 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
-import { getCustomerWishlist, addWishlistItem, removeWishlistItem, Wishlist } from '../../api/customers'
+import httpClient from '../../api/httpClient'
+import { getCustomerWishlist, addWishlistItem, removeWishlistItem, transferWishlistItemToOrder, Wishlist } from '../../api/customers'
 
 const route = useRoute()
 const customerId = route.params.id as string
 const wishlist = ref<Wishlist>({ id: '', customerId, productIds: [] })
+const products = ref<any[]>([])
 const newProductId = ref('')
 
 async function fetchWishlist() {
   const response = await getCustomerWishlist(customerId)
   wishlist.value = response.data
+  products.value = await Promise.all(
+    wishlist.value.productIds.map(async (id) => {
+      try {
+        return await httpClient.get<any>(`/products/${id}`)
+      } catch {
+        return { id, title: id }
+      }
+    })
+  )
 }
 
 async function addItem() {
@@ -38,6 +50,11 @@ async function addItem() {
 
 async function removeItem(id: string) {
   await removeWishlistItem(customerId, id)
+  await fetchWishlist()
+}
+
+async function transferToOrder(id: string) {
+  await transferWishlistItemToOrder(customerId, id)
   await fetchWishlist()
 }
 
