061e20db16b2b73c6c3c8ccb3b572363c7db814c
Miyuru Sangith Dharmage <voxsar@gmail.com>
2025-08-31 19:04:28 +0530

    feat: add index4 page components

---
+++ frontend/src/views/Index4404.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Header />
+    <div v-html="htmlContent"></div>
+    <Footer />
+  </div>
+</template>
+
+<script setup>
+import Header from '@/components/Header.vue';
+import Footer from '@/components/Footer.vue';
+import raw from '../../../website/index4-404.html?raw';
+
+const htmlContent = (() => {
+  let content = raw.split('<div id="sns_content"')[1] || '';
+  content = '<div id="sns_content"' + content;
+  content = content.split('<div id="sns_footer"')[0] || content;
+  return content;
+})();
+</script>
+
+<style lang="less">
+@import '@/assets/less/content.less';
+</style>
